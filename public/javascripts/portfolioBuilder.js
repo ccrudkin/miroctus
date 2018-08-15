facac8b4136e19520b5a4cd5c1926ace1d7c1fc0
ccrudkin <ccrudkin@gmail.com>
2018-08-15 17:23:02 -0600

    portfolio recommend functionality

--- public/javascripts/portfolioBuilder.js
+++ public/javascripts/portfolioBuilder.js
@@ -1,5 +1,15 @@
 let userData = JSON.parse(sessionStorage.getItem('investmentProfile'));
 
+const riskRate = {
+    0: 'conservative',
+    1: 'moderately_conservative',
+    2: 'moderate',
+    3: 'moderately_aggressive',
+    4: 'aggressive'
+}
+
+let riskNum = riskRate[userData[7]];
+
 document.getElementById('userProfile').innerHTML = 
     `<span class="sizeUp">Profile:</span>
     <span>Age: ${userData[0]}</span>
@@ -11,17 +21,33 @@ document.getElementById('userProfile').innerHTML =
     <span>Net worth: $${parseFloat(userData[6]).toLocaleString()}</span>
     <span>Risk willingness: ${userData[7]}</span>`;
 
-document.getElementById('recPort').innerHTML = 
-    ``
+function recommendations() {
+    $.ajax({
+        url: `/portfoliobuilder/${riskNum}`,
+        type: 'GET',
+        error(jqXHR, textStatus, errorThrown) {
+            console.log(errorThrown);
+        },
+        success(data, textStatus, jqXHR) {
+            document.getElementById('allocationChart').innerHTML = 
+            `<div class="sizeUp">${riskNum.slice(0, 1).toUpperCase() + riskNum.slice(1)} Portfolio</div>
+            <div class="flexH" id="allocationDetails">
+                <img src="/images/charts/${riskNum}.png" alt="${riskNum} allocation chart">
+                <span class="sizeDown flexV" id="allocationDetails">
+                    <span>${data.cash}</span>
+                    <span>${data.stb}</span>
+                    <span>${data.itb}</span>
+                    <span>${data.ltb}</span>
+                    <span>${data.lcvs}</span>
+                    <span>${data.lcgs}</span>
+                    <span>${data.mcs}</span>
+                    <span>${data.scs}</span>
+                    <span>${data.ids}</span>
+                    <span>${data.ies}</span>
+                </span>
+            </div>`;
+        }
+    });
+}
 
-/*
-<span class="sizeUp">Profile:</span>
-<span>Age: <%= userData[0] %></span>
-<span>Retirement age: <%= userData[1] %></span>
-<span>Initial investment: $<%= userData[2].toLocaleString() %></span>
-<span>Monthly savings: $<%= userData[3] %></span>
-<span>Annual income: $<%= userData[4] %></span>
-<span>Monthly expenses: $<%= userData[5] %></span>
-<span>Net worth: $<%= userData[6] %></span>
-<span>Risk willingness: <%= userData[7] %></span>
-*/
+recommendations();
--- routes/portfolioBuilderRouter.js
+++ routes/portfolioBuilderRouter.js
@@ -7,4 +7,72 @@ router.get('/', function(req, res) {
     // probably will store user info in a database in the future
 });
 
+router.get('/:portfolio', function(req, res) {
+    console.log(portfolios[req.params.portfolio])
+    res.send(portfolios[req.params.portfolio]);
+});
+
+const portfolios = {
+    'conservative': {
+        'cash': 'Cash: 5%',
+        'stb': 'Short Term Bonds: 47%',
+        'itb': 'Intermediate Term Bonds: 20%',
+        'ltb': 'Long Term Bonds: 0%',
+        'lcvs': 'Large Cap Value Stocks: 12%',
+        'lcgs': 'Large Cap Growth Stocks: 7%',
+        'mcs': 'Mid Cap Stocks: 0%',
+        'scs': 'Small Cap Stocks: 4%',
+        'ids': 'International Developed Stocks: 5%',
+        'ies': 'International Emerging Stocks: 0%'
+    },
+    'moderately_conservative': {
+        'cash': 'Cash: 4%',
+        'stb': 'Short Term Bonds: 32%',
+        'itb': 'Intermediate Term Bonds: 19%',
+        'ltb': 'Long Term Bonds: 0%',
+        'lcvs': 'Large Cap Value Stocks: 18%',
+        'lcgs': 'Large Cap Growth Stocks: 13%',
+        'mcs': 'Mid Cap Stocks: 0%',
+        'scs': 'Small Cap Stocks: 5%',
+        'ids': 'International Developed Stocks: 9%',
+        'ies': 'International Emerging Stocks: 0%'
+    },
+    'moderate': {
+        'cash': 'Cash: 4%',
+        'stb': 'Short Term Bonds: 22%',
+        'itb': 'Intermediate Term Bonds: 13%',
+        'ltb': 'Long Term Bonds: 0%',
+        'lcvs': 'Large Cap Value Stocks: 24%',
+        'lcgs': 'Large Cap Growth Stocks: 15%',
+        'mcs': 'Mid Cap Stocks: 0%',
+        'scs': 'Small Cap Stocks: 8%',
+        'ids': 'International Developed Stocks: 10%',
+        'ies': 'International Emerging Stocks: 0%'
+    },
+    'moderately_aggressive': {
+        'cash': 'Cash: 2%',
+        'stb': 'Short Term Bonds: 8%',
+        'itb': 'Intermediate Term Bonds: 8%',
+        'ltb': 'Long Term Bonds: 0%',
+        'lcvs': 'Large Cap Value Stocks: 30%',
+        'lcgs': 'Large Cap Growth Stocks: 21%',
+        'mcs': 'Mid Cap Stocks: 0%',
+        'scs': 'Small Cap Stocks: 13%',
+        'ids': 'International Developed Stocks: 12%',
+        'ies': 'International Emerging Stocks: 6%'
+    },
+    'aggressive': {
+        'cash': 'Cash: 0%',
+        'stb': 'Short Term Bonds: 0%',
+        'itb': 'Intermediate Term Bonds: 0%',
+        'ltb': 'Long Term Bonds: 0%',
+        'lcvs': 'Large Cap Value Stocks: 36%',
+        'lcgs': 'Large Cap Growth Stocks: 25%',
+        'mcs': 'Mid Cap Stocks: 0%',
+        'scs': 'Small Cap Stocks: 14%',
+        'ids': 'International Developed Stocks: 17%',
+        'ies': 'International Emerging Stocks: 8%'
+    }
+}
+
 module.exports = router;
